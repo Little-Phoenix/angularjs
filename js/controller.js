75f1323e944e8e085e3e03287ab50d47b490e9b6
Little Phoenix <532911080@qq.com>
2016-04-19 16:16:48 +0800

    add some examples

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,48 @@
+var app = angular.module("myApp",[]);
 function PhoneListCtrl($scope){
   $scope.phones = [
     {"name": "Nexus S",
-  "snippet": "Fast just got faster with Nexus S.",
-  "age": 1},
-  {"name": "Motorola XOOM with Wi-Fi",
-  "snippet": "The Next, Next Generation tablet.",
-  "age": 42},
-  {"name": "MOTOROLA XOOM1",
-  "snippet": "The Next, Next Generation tablet.",
-  "age": 23}
+    "snippet": "Fast just got faster with Nexus S.",
+    "age": 1},
+    {"name": "Motorola XOOM with Wi-Fi",
+    "snippet": "The Next, Next Generation tablet.",
+    "age": 42},
+    {"name": "MOTOROLA XOOM1",
+    "snippet": "The Next, Next Generation tablet.",
+    "age": 23}
 ];
+}
+
+
+function customersCtrl($scope, $location){
+  $scope.myUrl = $location.absUrl();
+}
+
+
+function timeoutCtrl($scope, $timeout){
+  $scope.myHeader = "Hello World!";
+  $timeout(function(){
+    $scope.myHeader = "How are you today?";
+  }, 2000);
+}
+
+
+app.service('hexify', function() {
+  this.myFunc = function(x){
+    return x.toString(16);
+  }
+});
+
+function zidingyiCtrl($scope, hexify){
+  $scope.hex = hexify.myFunc(255);
+}
+
+app.filter('myFormat', ['hexify', function(hexify){
+  return function(x){
+    return hexify.myFunc(x);
+  }
+}])
+
+function filterCtrl($scope){
+  $scope.counts = [255,251,200];
 }
