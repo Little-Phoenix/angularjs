ebcdec88e25cc7f88789a85aa3f7b94537725ce2
Little Phoenix <532911080@qq.com>
2016-04-21 17:01:09 +0800

    remove describe

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 function addTodoInfo($scope){
   $scope.todoTitle = "";
-  $scope.describe = "";
   $scope.todoList = [];
   $scope.addInfo = function(){
     if($.trim($scope.todoTitle) != ""){
-      $scope.todoList.push({"title": $scope.todoTitle, "describe": $scope.describe});
+      $scope.todoList.push({"title": $scope.todoTitle});
     }
     $scope.todoTitle = "";
-    $scope.describe = "";
   }
 
   $scope.delInfo = function(one){
     $.each($scope.todoList,function(i,item){
-      if(item.todoTitle == one.todoTitle && item.describe == one.describe){
+      if(item.todoTitle == one.todoTitle){
         $scope.todoList.splice(i,1);
         return false;
       }
