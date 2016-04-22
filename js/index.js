51550d8cbc05f662ba2c3c75718d2119ad19d323
Little Phoenix <532911080@qq.com>
2016-04-22 16:53:13 +0800

    增加时间标识，并将所有的list存储到localstorage中

--- js/index.js
+++ js/index.js
@@ -3,9 +3,42 @@ function addTodoInfo($scope){
   $scope.todoList = [];
   $scope.isDel = false;
   $scope.isRec = true;
+  //初始化，初始化todoList
+  $scope.init = function(){
+    var keys = localStorage.getItem("keys");
+    if(keys){
+      var keyArr = keys.split(",");
+      $.each(keyArr,function(i,key){
+        var oneStr = localStorage.getItem(key);
+        console.log(oneStr);
+        if(oneStr){
+          var one = JSON.parse(oneStr);
+          $scope.todoList.push(one);
+        }
+      });
+    }
+  }
   $scope.addInfo = function(){
     if($.trim($scope.todoTitle) != ""){
-      $scope.todoList.push({"title": $scope.todoTitle,"isDel": false,"isRec": true});
+      var currentTime = new Date();
+      var one = {"title": $scope.todoTitle,
+                 "isDel": $scope.isDel,
+                 "isRec": $scope.isRec,
+                 "createTime": currentTime,
+                 "updateTime": currentTime,
+                 "key": currentTime.getTime()};
+      $scope.todoList.push(one);
+
+      var keys = localStorage.getItem("keys");
+      if(!keys){
+        keys = currentTime.getTime();
+      }else{
+        keys += "," + currentTime.getTime();
+      }
+
+      //保存值
+      localStorage.setItem("keys", keys);
+      localStorage.setItem(currentTime.getTime(), JSON.stringify(one));
     }
     $scope.todoTitle = "";
   }
@@ -13,9 +46,16 @@ function addTodoInfo($scope){
   $scope.delInfo = function(one){
     one.isDel = true;
     one.isRec = false;
+    one.updateTime = new Date();
+    localStorage.setItem(one.key, JSON.stringify(one));
   }
   $scope.recInfo = function(one){
     one.isDel = false;
     one.isRec = true;
+    one.updateTime = new Date();
+    localStorage.setItem(one.key, JSON.stringify(one));
   }
+
+  $scope.init();
+
 }
