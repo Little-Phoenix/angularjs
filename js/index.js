44fba7bd4fcf3cb84043d03894a14de7f7a469ed
Little Phoenix <532911080@qq.com>
2016-04-22 12:36:05 +0800

    增加逻辑删除功能

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 function addTodoInfo($scope){
   $scope.todoTitle = "";
   $scope.todoList = [];
+  $scope.isDel = false;
+  $scope.isRec = true;
   $scope.addInfo = function(){
     if($.trim($scope.todoTitle) != ""){
-      $scope.todoList.push({"title": $scope.todoTitle});
+      $scope.todoList.push({"title": $scope.todoTitle,"isDel": false,"isRec": true});
     }
     $scope.todoTitle = "";
   }
 
   $scope.delInfo = function(one){
-    $.each($scope.todoList,function(i,item){
-      if(item.todoTitle == one.todoTitle){
-        $scope.todoList.splice(i,1);
-        return false;
-      }
-    });
+    one.isDel = true;
+    one.isRec = false;
   }
-
+  $scope.recInfo = function(one){
+    one.isDel = false;
+    one.isRec = true;
+  }
 }
